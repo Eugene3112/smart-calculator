71125bf2c6874a052558388ef65a13fe83a440c3
Eugene3112 <bertoshevgieniy@gmail.com>
2018-10-07 18:35:11 +0300

    commit

--- src/index.js
+++ src/index.js
@@ -1,26 +1,34 @@
 class SmartCalculator {
   constructor(initialValue) {
-    // your implementation
+    this.initialValue = initialValue;
   }
 
   add(number) {
-    // your implementation
+    this.initialValue += '+' + number;
+    return this;
   }
   
   subtract(number) {
-    // your implementation
+    this.initialValue += '-' + number;
+    return this;
   }
 
   multiply(number) {
-    // your implementation
+    this.initialValue += '*' + number;
+    return this;
   }
 
   devide(number) {
-    // your implementation
+    this.initialValue += '/' + number;
+    return this;
   }
 
   pow(number) {
-    // your implementation
+    this.initialValue += '**' + number;
+    return this;
+  }
+  valueOf() {
+    return eval(this.initialValue);
   }
 }
 
